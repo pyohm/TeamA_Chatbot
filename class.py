c53bc9a735a6e1cf3d1a8ae1222bf723697c4eb6
GoDaeYun <eodbs117848@gmail.com>
2024-11-29 19:43:22 +0900

    설치환경

--- class.py
+++ class.py
@@ -6,7 +6,7 @@ import PyPDF2
 import gradio as gr
 import base64
 
-#pip install requirements.txt 설치후
+#pip install -r requirements.txt 설치후
 #python -m spacy download ko_core_news_sm 설치후 사용 
 
 class IMAGE:
